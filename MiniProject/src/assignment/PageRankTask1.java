9380356faa825911132e57b9dc067925eb8dedc0
essaic <vivemadaal@gmail.com>
2013-10-19 13:22:39 +0200

    Working on Task 1.c

--- MiniProject/src/assignment/PageRankTask1.java
+++ MiniProject/src/assignment/PageRankTask1.java
@@ -10,13 +10,13 @@ public class PageRankTask1 {
 	public static void main(String[] argv) {   
 		/*Réseau de pages exemple*/
 		int [][] net = {
-			{ 1, 2 },    //page 0
-			{ 2, 2, 4},  //page 1
-			{ 4 },       //page 2
-			{ 0, 0},     //page 3
-			{ 1, 2 , 4}  //page 4
+				{ 1, 2 },    //page 0
+				{ 2, 2, 4},  //page 1
+				{ 4 },       //page 2
+				{ 0, 0},     //page 3
+				{ 1, 2 , 4}  //page 4
 		};
-		
+
 		int[] path = randomSurfer(net, 10);
 		for (int i = 0; i < path.length; i++) {
 			System.out.println("L'utilisateur visite la page " + path[i]);
@@ -29,50 +29,58 @@ public class PageRankTask1 {
 
 	public static int[] randomSurfer(int[][] net, int steps) {  
 		/* Méthode à coder */
-		
+
 		int[] pages = new int[steps];
-		
-		//on commence a la page 0 
-		pages[0] = 0;
-		
+
+		if(net.length > 0 && steps > 0)
+		{
+			//We start on page 0
+			pages[0] = 0;
+		}
+
 		for(int i = 1; i < steps; ++i)
 		{
 			pages[i] = getNextPage(net, pages[i-1]);
 		}
-		
+
 		return pages;
 	}
-	
+
 	public static int getNextPage(int[][] net, int currentPage) {
 		/* Méthode à coder
 		 * Utilisez random.nextDouble() pour générer un réel aléatoire
 		 * et random.nextInt(int n) pour générer un entier aléatoire.
 		 */
-		
+
+		//The damping coefficient
 		double damping = 0.9;
-		
+
 		double dampingChoice = random.nextDouble();
+
+		int[] pageLinks = net[currentPage];
 		
-		if(dampingChoice > damping)
+		if(dampingChoice < damping && pageLinks.length != 0)
 		{
-			//On saute alors vers une page aleatoire
-			return random.nextInt(net.length);
+			//Take a random links from the page
+			
+			
+			int linkChoice = random.nextInt(pageLinks.length);
+
+			return pageLinks[linkChoice];
 		}
 		else
 		{
-			//On prend un lien de la page de face aleatoire
-			int[] pageLinks = net[currentPage];
-			int linkChoice = random.nextInt(pageLinks.length);
-			
-			return pageLinks[linkChoice];
+			//Take a random url int the whole network
+			return random.nextInt(net.length);
 		}
+
 	}
 
 	public static String visualizeVisit(int page, int totalPageNum) {
 		/* Méthode à coder */
-		
+
 		String result = "";
-		
+
 		for(int i = 0; i < totalPageNum; i++)
 		{
 			if(i ==  page)
@@ -84,7 +92,7 @@ public class PageRankTask1 {
 				result += "-";
 			}
 		}
-		
+
 		return result;
 	}
 }
