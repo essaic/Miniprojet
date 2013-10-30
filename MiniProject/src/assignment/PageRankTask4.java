4dc92030c7aedd64509fc660f085ec0768f3ec5f
30000Chopin <lauriane.mollier@gmail.com>
2013-10-30 15:24:51 +0100

    Update PageRankTask4.java

--- MiniProject/src/assignment/PageRankTask4.java
+++ MiniProject/src/assignment/PageRankTask4.java
@@ -5,52 +5,102 @@ import java.util.Random;
 
 public class PageRankTask4 {
 
-	/*
-	Réponses à la question de la tâche 4.d :
-		- Quelle méthode nécessite le moins d'itérations pour obtenir
-		un PageRank précis ? 
-		- Réponse :
-		
-		- Pourquoi, selon vous ?
-		- Réponse :
-		
-	*/
+        /*
+        RÉPONSES À LA QUESTION DE LA TÂCHE 4.D :
+                - QUELLE MÉTHODE NÉCESSITE LE MOINS D'ITÉRATIONS POUR OBTENIR
+                UN PAGERANK PRÉCIS ?
+                - RÉPONSE :
+                
+                - POURQUOI, SELON VOUS ?
+                - RÉPONSE :
+                
+        */
 
-	/* Utilisez cet objet pour générer des nombres aléatoires*/
-	public static Random random = new Random(2013);    
-	
-	public static void main(String[] argv) {   
-		/*Réseau de pages exemple*/
-		int [][] net = {
-			{ 1, 2 },    //page 0
-			{ 2, 2, 4},  //page 1
-			{ 4 },       //page 2
-			{ 0, 0},     //page 3
-			{ 1, 2 , 4}  //page 4
-		};
-		
-		System.out.println("Estimation du PageRank (random walk - 25 itérations - damping 0.9) : ");
-		int[] path = PageRankTask1.randomSurfer(net, 25);
-		System.out.println(Arrays.toString(PageRankTask3.computePageRank(path, net.length)));
+        /* UTILISEZ CET OBJET POUR GÉNÉRER DES NOMBRES ALÉATOIRES*/
+        
+        PUBLIC STATIC VOID MAIN(STRING[] ARGV) {
+                /*RÉSEAU DE PAGES EXEMPLE*/
+                INT [][] NET = {
+                        { 1, 2 }, //PAGE 0
+                        { 2, 2, 4}, //PAGE 1
+                        { 4 }, //PAGE 2
+                        { 0, 0}, //PAGE 3
+                        { 1, 2 , 4} //PAGE 4
+                };
+                
+                SYSTEM.OUT.PRINTLN("ESTIMATION DU PAGERANK (RANDOM WALK - 25 ITÉRATIONS - DAMPING 0.9) : ");
+                INT[] PATH = PAGERANKTASK1.RANDOMSURFER(NET, 25);
+                SYSTEM.OUT.PRINTLN(ARRAYS.TOSTRING(PAGERANKTASK3.COMPUTEPAGERANK(PATH, NET.LENGTH)));
 
-		System.out.println("Estimation du PageRank (méthode matricielle) - 25 itérations - damping 0.9 : ");
-		System.out.println(Arrays.toString(estimatePageRank(net, 25, 0.9)));	
-	}
+                SYSTEM.OUT.PRINTLN("ESTIMATION DU PAGERANK (MÉTHODE MATRICIELLE) - 25 ITÉRATIONS - DAMPING 0.9 : ");
+                SYSTEM.OUT.PRINTLN(ARRAYS.TOSTRING(ESTIMATEPAGERANK(NET, 25, 0.9)));    
+                
+        }
 
 
-	public static double[] estimatePageRank(int[][] net, int steps, double dampingFactor) {
-		/* Méthode à coder */
-		return null;
-	}
+        PUBLIC STATIC DOUBLE[] ESTIMATEPAGERANK(INT[][] NET, INT STEPS, DOUBLE DAMPINGFACTOR) {
+                /* MÉTHODE À CODER */
+        
+                RETURN PAGERANKITERATIONS(COMPUTETRANSITIONSMATRIX(NET,DAMPINGFACTOR), STEPS);
+        }
 
-	public static double[][] computeTransitionsMatrix(int[][] net, double dampingFactor) {
-		/* Méthode à coder */
-		return null;
-	}
+        PUBLIC STATIC DOUBLE[][] COMPUTETRANSITIONSMATRIX(INT[][] NET, DOUBLE DAMPINGFACTOR) {
+                /* MÉTHODE À CODER */
+                
+                DOUBLE[][] TRANSITIONS = NEW DOUBLE[NET.LENGTH][NET.LENGTH];
+                
+                FOR(INT I = 0; I<NET.LENGTH; ++I ){ 
+					FOR(INT J = 0; J<NET[I].LENGTH; ++J ){
+						TRANSITIONS[I][NET[I][J]]++;
+					}
+				}
+                
+                FOR(INT I = 0; I<NET.LENGTH; ++I ){
+                	INT NBPAGEREF = 0;
+                	FOR(INT K = 0; K <NET.LENGTH; ++K ){
+						IF(TRANSITIONS[I][K] != 0){
+							++NBPAGEREF;
+						}
+                	}
+					FOR(INT J = 0; J<NET.LENGTH; ++J ){
+						TRANSITIONS[I][J] = TRANSITIONS[I][J] * DAMPINGFACTOR / NBPAGEREF  + (1 - DAMPINGFACTOR)/NET.LENGTH;
+					}
+				}    
+                
+      
+                
+                RETURN TRANSITIONS;
+        }
 
-	public static double[] pageRankIterations(double[][] transitions, int steps) {
-		/* Méthode à coder */
-		return null;
-	}
+        PUBLIC STATIC DOUBLE[] PAGERANKITERATIONS(DOUBLE[][] TRANSITIONS, INT STEPS) {
+                /* METHODE À CODER */
+        		DOUBLE[] TABLE = NEW DOUBLE[TRANSITIONS.LENGTH];
+        		TABLE[0] = 1.0;
+        		
+        		
+        		FOR(INT PAS = 0; PAS <STEPS; ++PAS){
+        			DOUBLE[] TABLEDECALCUL = NEW DOUBLE[TRANSITIONS.LENGTH];
+        			
+        			FOR(INT I=0; I<TRANSITIONS.LENGTH; ++I){
+        				TABLEDECALCUL[I] = TABLE[I];
+        			}
+     
+        			FOR(INT I= 0; I < TRANSITIONS.LENGTH; ++I){
+        				DOUBLE SOMME = 0.0;
+        				FOR(INT J= 0; J < TRANSITIONS.LENGTH; ++J){
+            				SOMME += TABLE[J] * TRANSITIONS[J][I];
+            			}
+        				TABLEDECALCUL[I]= SOMME;
+        			}
+        			
+        			FOR(INT I=0; I<TRANSITIONS.LENGTH; ++I){
+        				TABLE[I] = TABLEDECALCUL[I];	
+        			}
+        			
+        		}
+        	
+        			 
+        		RETURN TABLE;
+        }
 
 }
